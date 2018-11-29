f1c39b18b750537a0ee8903c870e75317e64e372
Marcelolac <44480740+Marcelolac@users.noreply.github.com>
2018-11-29 11:11:52 -0200

    Versão do professor para fazer a última parte

--- src/main/java/br/edu/ulbra/election/party/client/CandidateClientService.java
+++ src/main/java/br/edu/ulbra/election/party/client/CandidateClientService.java
@@ -1,32 +1,32 @@
 package br.edu.ulbra.election.party.client;
 
-import java.util.List;
-
+import br.edu.ulbra.election.party.output.v1.CandidateOutput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import br.edu.ulbra.election.party.output.v1.CandidateOutput;
+import java.util.List;
 
 @Service
 public class CandidateClientService {
-	private final CandidateClient candidateClient;
+
+    private final CandidateClient candidateClient;
 
     @Autowired
-    public CandidateClientService(CandidateClient candidateClient) {
+    public CandidateClientService(CandidateClient candidateClient){
         this.candidateClient = candidateClient;
     }
 
-    public List<CandidateOutput> getByPartyId(Long partyId) {
-        return this.candidateClient.getByPartyId(partyId);
+    public List<CandidateOutput> getByParty(Long partyId){
+        return candidateClient.getByParty(partyId);
     }
 
-    @FeignClient(value="candidate-service", url="${url.candidate-service}")
+    @FeignClient(name = "candidate-service", url = "${url.candidate-service}")
     private interface CandidateClient {
 
         @GetMapping("/v1/candidate/party/{partyId}")
-        List<CandidateOutput> getByPartyId(@PathVariable(name = "partyId") Long partyId);
+        List<CandidateOutput> getByParty(@PathVariable(name = "partyId") Long partyId);
     }
 }
